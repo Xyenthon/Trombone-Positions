b3a049866731fa0f410189535fec3ec5441c1bbc
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-09-27 16:31:07 -0400

    I rewrote your thing and made it better.

--- js/main.js
+++ js/main.js
@@ -1,69 +1,46 @@
 //vars and other important stuff
-var fundamental = ['Bb','A','Ab','G','F#','F','E'];
-var BaseFreq = [58.27,55,51.91,49,46.25,43.65,41.2];
-var position;
-var partial;
-var PartialFreq = partial * BaseFreq[position-1]; 
+var A = 440; // tuning, I think I'm funny and clever (get it: A = 440? eh? no one? ok, I'll stop)
 
-//Messy but I really dont care
-var pain = (Math.abs(Math.floor(PartialFreq-174.61)/100)+1;
-
-function octave(PartialFreq)
-{
-  var octavenum;
-  /*
-  First if statement is ridiculous and absolutly unnessary, which is why I put it there.
-  
- Also, I'm only using a million if fuctions cause im too lazy to find the equation for the frequency distribution of octaves and actually implement it.
- btw: The distribution is a ratio of 2:1, but im not putting it in cause im too lazy.
-  */
-  if (PartialFreq >= 4.09 && PartialFreq < 8.18)
-  {
-    octavenum = -2;
-  }
-  else if (PartialFreq >= 8.18 && PartialFreq < 16.35)
-  {
-    octavenum = -1;
-  }
-  else if (PartialFreq >= 16.35 && PartialFreq < 32.7)
-  {
-    octavenum = 0;
-  }
-  else if (PartialFreq >= 32.7 && PartialFreq < 65.41)
-  {
-    octavenum = 1;
-  }
-  else if (PartialFreq >= 65.41 && PartialFreq < 130.81)
-  {
-    octavenum = 2;
-  }
-  else if (PartialFreq >= 130.81 && PartialFreq < 261.63)
-  {
-    octavenum = 3;
-  }
-  else if (PartialFreq >= 261.63 && PartialFreq < 523.25)
-  {
-    octavenum = 4;
-  }
-  else if (PartialFreq >= 523.25 && PartialFreq < 1046.5)
-  {
-    octavenum = 5;
-  }
-  //after this point, just play the flute and/or go to the hospital.
-  
-  
-  //                   Hey look its a whole number |
-  //                                               v
-  else if (PartialFreq >= 1046.5 && PartialFreq < 2093)
-  {
-    octavenum = 6;
-  }
-  else if (PartialFreq >= 2093 && PartialFreq < 4186)
-  {
-    octavenum = 7;
-  }
-  //else (go back to later cause I dont want to do this error thingie now)
-  
+// key = note name, value = semitones above A. If you want, I can golf this down a lot, but I like it best as a lookup table.
+// used by routines
+var semitones = {
+   'A': 0,
+  'Bb': 1,
+  'A#': 1,
+  'B': 2,
+  'C': 3,
+  'Db': 4,
+  'C#': 4,
+  'D': 5,
+  'Eb': 6,
+  'D#': 6,
+  'E': 7,
+  'F': 8,
+  'Gb': 9,
+  'F#': 9,
+  'G': 10,
+  'Ab': 11,
+  'G#': 11
 }
 
+var fundamental = ['Bb','A','Ab','G','F#','F','E'];
+var baseFreq = fundamental.map(note=>freq(note, 1)) // avoid magic numbers!
+var position = 1, partial = 1;
+var partialFreq = partial * baseFreq[position-1];
 
+//Messy but I really dont care
+var pain = Math.abs(Math.floor(partialFreq-174.61)/100)+1); // this makes no sense, what are you trying to do?
+
+// freq: frequency of note
+function octave(freq) {
+  return  Math.floor(Math.log2(freq/16.35))
+}
+
+/*
+note: something like 'C', 'Bb', 'F#', etc.
+octave: octaves above A4
+*/
+function freq(note, octave) {
+  var steps = (octave - 4) * 12 + semitones[note];
+  return Math.round(A * Math.pow(Math.pow(2,1/12),steps),9) // rounding to get rid of those darn floating point errors :)
+}
