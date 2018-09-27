3e05add28283f7f6d5a72bacfdd7dfdf97900584
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-09-27 16:33:23 -0400

    Code beautification
    Pro tip: use https://beautifier.io/ with the appropriate settings (2 spaces for indent, space before conditional, unescape printable chars , use JSLint-happy formatting tweaks)

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var A = 440; // tuning, I think I'm funny and clever (get it: A = 440? eh? no on
 // key = note name, value = semitones above A. If you want, I can golf this down a lot, but I like it best as a lookup table.
 // used by routines
 var semitones = {
-   'A': 0,
+  'A': 0,
   'Bb': 1,
   'A#': 1,
   'B': 2,
@@ -23,17 +23,18 @@ var semitones = {
   'G#': 11
 }
 
-var fundamental = ['Bb','A','Ab','G','F#','F','E'];
-var baseFreq = fundamental.map(note=>freq(note, 1)) // avoid magic numbers!
-var position = 1, partial = 1;
-var partialFreq = partial * baseFreq[position-1];
+var fundamental = ['Bb', 'A', 'Ab', 'G', 'F#', 'F', 'E'];
+var baseFreq = fundamental.map(note => freq(note, 1)) // avoid magic numbers!
+var position = 1,
+  partial = 1;
+var partialFreq = partial * baseFreq[position - 1];
 
 //Messy but I really dont care
-var pain = Math.abs(Math.floor(partialFreq-174.61)/100)+1); // this makes no sense, what are you trying to do?
+var pain = Math.abs(Math.floor(partialFreq - 174.61) / 100) + 1); // this makes no sense, what are you trying to do?
 
 // freq: frequency of note
 function octave(freq) {
-  return  Math.floor(Math.log2(freq/16.35))
+  return Math.floor(Math.log2(freq / 16.35))
 }
 
 /*
@@ -42,5 +43,5 @@ octave: octaves above A4
 */
 function freq(note, octave) {
   var steps = (octave - 4) * 12 + semitones[note];
-  return Math.round(A * Math.pow(Math.pow(2,1/12),steps),9) // rounding to get rid of those darn floating point errors :)
+  return Math.round(A * Math.pow(Math.pow(2, 1 / 12), steps), 9) // rounding to get rid of those darn floating point errors :)
 }
